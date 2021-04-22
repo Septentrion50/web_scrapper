09e56ff0c813f599e14663bcd8634d7e2b875545
Gael <gmeanti@gmail.com>
2021-04-22 11:12:35 +0200

    Scrap

--- lib/web_scrapper.rb
+++ lib/web_scrapper.rb
@@ -1,1 +1,27 @@
-# This is a comment
+require 'nokogiri'
+require 'open-uri'
+
+
+def scraping 
+    url = "https://coinmarketcap.com/all/views/all/"
+    page = Nokogiri::HTML(open(url))
+
+    
+    //*[@id="__next"]/div/div[1]/div[2]/div/div/div[2]/table/tbody/tr[1]/td[3]/div/a/div/div/p
+    nom = []
+    vameur = [] 
+
+
+
+    combo.each do |crypto|
+        puts 
+    end
+
+
+end
+
+
+
+perform
+    adresse
+end

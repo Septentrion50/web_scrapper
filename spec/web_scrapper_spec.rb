cd01cb5e5fb8e6e91f6735069adebe6c32d49071
Septentrion50 <erwan56lecorre@gmail.com>
2021-04-22 11:05:38 +0200

    Ajout du test pour la méthode crypto_scrapper

--- spec/web_scrapper_spec.rb
+++ spec/web_scrapper_spec.rb
@@ -1,1 +1,12 @@
-# This is a comment
+require_relative '../lib/web_scrapper'
+
+describe "Tests for our web_scrapper program" do
+
+  describe "Crypto_scrapper" do
+    it "Tests if the output is a non-nil hash" do
+      expect(crypto_scrapper).to be_a_kind_of(Array)
+      expect(crypto_scrapper).not_to be_empty
+    end
+  end
+
+end

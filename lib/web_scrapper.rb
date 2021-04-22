c74e302e8ec904ab9775b37509db0af1fabca151
Septentrion50 <erwan56lecorre@gmail.com>
2021-04-22 15:09:19 +0200

    Ajout de la méthode mairie_christmas

--- lib/web_scrapper.rb
+++ lib/web_scrapper.rb
@@ -13,4 +13,21 @@ def crypto_scrapper
     return out
 end
 
-puts crypto_scrapper
+def mairie_christmas
+  page_general = Nokogiri::HTML(URI.open("http://annuaire-des-mairies.com/95/"))
+  links = page_general.css('pre/a')
+  out = []
+  links.each do |element|
+    begin
+      page = Nokogiri::HTML(URI.open("http://annuaire-des-mairies.com/95/#{element.text}"))
+      name = page.css('main//h1').text
+      table = page.css('table.table:nth-child(1)')
+      mail = table.css('tbody/tr[4]/td[2]').text
+      out << Hash[name[0..-9].downcase=>mail] 
+    rescue OpenURI::HTTPError, URI::InvalidURIError
+      puts "Not a valid link"
+    end
+  end
+  return  out.delete_if {|x| x == {""=>""}}
+end
+
